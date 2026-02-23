8581c0d02acb8da18bcdf925b7eede7456a501d3
Durvish <145188086+DurvishJ@users.noreply.github.com>
2026-02-22 20:48:58 -0500

    Refer to note
    - added start button
    - Added replay game button
    - Added music
    - Added "enter" key shortcut to start game
    - Added blur background to start button, for cooler effects

--- runner-rush/main.gd
+++ runner-rush/main.gd
@@ -4,11 +4,13 @@ extends Node
 @onready var timer = $DeliveryTime
 @onready var timerLabel = $CanvasLayer/Timer
 @onready var gameOver = $CanvasLayer/GameOver
+@onready var start_button = $CanvasLayer/StartButton
 
 var elapsed_time = 0.0
 
 func _ready() -> void:
-	new_game()
+	get_tree().paused = true # This freezes everything initially
+
 
 func _process(delta: float) -> void:
 	if elapsed_time >= 45:
@@ -19,23 +21,56 @@ func _process(delta: float) -> void:
 		elapsed_time += delta
 		timerLabel.text = "%.2f" % elapsed_time
 	
-
 func new_game():
+	get_tree().paused = false  # This "wakes up" the timer and customers
+	elapsed_time = 0.0         # Resets the clock to 0.00
+	gameOver.text = ""
 	$DeliveryTime.start()
 	$Player.start($StartPosition.position)
+	$Music.play()
 
 
 func _on_finish_body_entered(body: Node2D) -> void:
+	get_tree().paused = true
 	if elapsed_time > 45:
 		$LoseGame.play()
 		gameOver.text = "You lose"
 	else:
 		gameOver.text = "You win!"
+		
+	await get_tree().create_timer(2.0).timeout
+	
+	start_button.text = "Play Again"
+	start_button.show()
+	$CanvasLayer/BlurBackground.show()
+	
 	get_tree().paused = true
 
 
 func _on_chatty_customer_hit() -> void:
+	get_tree().paused = true
 	$LoseGame.play()
 	gameOver.text = "You lose"
-	get_tree().paused = true
+	
+	await get_tree().create_timer(2.0).timeout
+	
+	start_button.text = "Play Again"
+	start_button.show()
+	$CanvasLayer/BlurBackground.show()
+	
+
+	
+	
+func _on_start_button_pressed() -> void:
+	# If the button says "Play Again", reload everything
+	if start_button.text == "Play Again":
+		get_tree().paused = false 
+		get_tree().reload_current_scene()
+	else:
+		# If it's the very first start, just run the normal start logic
+		start_button.hide()
+		$CanvasLayer/BlurBackground.hide()
+		new_game()
+
+
 	
